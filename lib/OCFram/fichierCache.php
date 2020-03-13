6666b0af31d496a8648c42cc339a4e73a31c6b63
john-rembrandt <sk8mystic@gmail.com>
2020-03-13 23:15:25 +0100

    changer le nom du fichier et l extension

---
+++ lib/OCFram/fichierCache.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      dest test et des test     </title>
+    
+    <meta charset="utf-8" />
+    
+    <link rel="stylesheet" href="/css/Envision.css" type="text/css" />
+  </head>
+  
+  <body>
+    <div id="wrap">
+      <header>
+        <h1><a href="/">Mon super site</a></h1>
+        <p>Comment ça, il n'y a presque rien ?</p>
+      </header>
+      
+      <nav>
+        <ul>
+          <li><a href="/">Accueil</a></li>
+                  </ul>
+      </nav>
+      
+      <div id="content-wrap">
+        <section id="main">
+                    
+          <p>Par <em>robert</em>, le 11/02/2018 à 18h51</p>
+<h2>dest test et des test </h2>
+<p>analyser sous tout les angles</p>
+
+
+<p><a href="commenter-5.html">Ajouter un commentaire</a></p>
+
+<p>Aucun commentaire n'a encore été posté. Soyez le premier à en laisser un !</p>
+
+<p><a href="commenter-5.html">Ajouter un commentaire</a></p>        </section>
+      </div>
+    
+      <footer></footer>
+    </div>
+  </body>
+</html>
