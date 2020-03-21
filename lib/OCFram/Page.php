d95f06aeb241f00d73eedb9007873c8f9528b5ba
john-rembrandt <sk8mystic@gmail.com>
2020-03-21 01:20:03 +0100

    modif adresse layout

--- lib/OCFram/Page.php
+++ lib/OCFram/Page.php
@@ -32,7 +32,7 @@ class Page extends ApplicationComponent
     $content = ob_get_clean();
 
     ob_start();
-      require __DIR__.'/../../App/'.$this->app->name().'/Templates/layout.php';
+      require __DIR__.'/../../App/'.$this->app->name().'/Templates/index.html';
     return ob_get_clean();
   }
 
