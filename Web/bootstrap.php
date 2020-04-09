8b57b60f644fd77cc0191728f81008b4391c2189
john-rembrandt <sk8mystic@gmail.com>
2020-04-10 00:07:43 +0100

    effacer les registers plus utilisé

--- Web/bootstrap.php
+++ Web/bootstrap.php
@@ -8,26 +8,6 @@ if (!isset($_GET['app']) || !file_exists(__DIR__.'/../App/'.$_GET['app'])) $_GET
 // on apppel l'autoload psr-4 générer par composer aprés ajout des namespaces au composer.json
 require '../vendor/autoload.php';
 
-/* 
-// On commence par inclure la classe nous permettant d'enregistrer nos autoload
-require __DIR__.'/../lib/OCFram/SplClassLoader.php';
-
-// On va ensuite enregistrer les autoloads correspondant à chaque vendor (OCFram, App, Model, etc.)
-$OCFramLoader = new SplClassLoader('OCFram', __DIR__.'/../lib');
-$OCFramLoader->register();
-
-$appLoader = new SplClassLoader('App', __DIR__.'/..');
-$appLoader->register();
-
-$modelLoader = new SplClassLoader('Model', __DIR__.'/../lib/vendors');
-$modelLoader->register();
-
-$entityLoader = new SplClassLoader('Entity', __DIR__.'/../lib/vendors');
-$entityLoader->register();
-
-$formBuilderLoader = new SplClassLoader('FormBuilder', __DIR__.'/../lib/vendors');
-$formBuilderLoader->register(); */
-
 
 // Il ne nous suffit plus qu'à déduire le nom de la classe et de l'instancier
 $appClass = 'App\\'.$_GET['app'].'\\'.$_GET['app'].'Application';
