31e17d28dca0c2fd713a9997331963a167c4c7e3
john-rembrandt <sk8mystic@gmail.com>
2020-11-10 18:32:50 +0100

    js sur eclipse catastrophe wwd pas stable retour a VSC

--- Web/test.js
+++ Web/test.js
@@ -2,7 +2,7 @@
 
 //var titre = document.getElementById(title);
 //var
-//document.getElementById('titre').innerHTML = '<blockquote>Je mets une citation à la place du paragraphe</blockquote>';
+document.getElementById('titre').innerHTML = '<blockquote>Je mets une citation à la place du paragraphe</blockquote>';
 
 $(function() {
   $('#titre').html('Hello world. Ce texte est affiché par jQuery.');
